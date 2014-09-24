ac070680d43e227e8a2830a9f2755cd66fa3792d
unknown <theshark-fb@hotmail.fr>
2014-09-24 11:16:48 +0200

    Update audio player

--- assets/js/audioPlayer.js
+++ assets/js/audioPlayer.js
@@ -1,15 +1,17 @@
-player = document.getElementById("audioPlayer");
-player.volume=0.5;
+player = document.getElementById('audioPlayer');
+player.volume=0.5; 
 
 function volume(type){
-  var player = document.querySelector('#audioPlayer');
+  var player = document.getElementById('audioPlayer');
+  var progress = document.getElementById('volumeProgress');
   var vol = player.volume;
   if(type == "minus"){
     if(vol - 0.1 >= 0)
-    player.volume = vol - 0.1;
+      player.volume = vol - 0.1;
   }
   if(type == "plus" && vol < 1){
     if(vol + 0.1 <= 1)
       player.volume = vol + 0.1;
   }
+  progress.value = player.volume;
 }
